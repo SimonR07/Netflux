1b4501db7e38184a9ecaafbcf90b6f625fc7dd92
Simon Rulquin <simon.rulquin@gmail.com>
2020-02-15 16:24:25 +0100

    data movies static

---
+++ src/data/movies.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    backdrop_path: './posters/emmanuelle.jpg',
+    id: 1,
+    overview:
+      'Emmanuelle s\'envole de Paris pour Bangkok afin d\'y rejoindre Jean, son mari qui occupe un poste de diplomate ; lorsqu\'il lui demande si elle a eu des amants pendant qu\'elle était seule à Paris, elle lui affirme que non. ',
+    poster_path: './posters/emmanuelle.jpg',
+    title: 'Emmanuelle',
+  },
+  {
+    backdrop_path: './posters/emmanuelle.jpg',
+    id: 2,
+    overview:
+      'Emmanuelle s\'envole de Paris pour Bangkok afin d\'y rejoindre Jean, son mari qui occupe un poste de diplomate ; lorsqu\'il lui demande si elle a eu des amants pendant qu\'elle était seule à Paris, elle lui affirme que non. ',
+    poster_path: './posters/emmanuelle.jpg',
+    title: 'Emmanuelle',
+  },
+  {
+    backdrop_path: './posters/emmanuelle.jpg',
+    id: 3,
+    overview:
+      'Emmanuelle s\'envole de Paris pour Bangkok afin d\'y rejoindre Jean, son mari qui occupe un poste de diplomate ; lorsqu\'il lui demande si elle a eu des amants pendant qu\'elle était seule à Paris, elle lui affirme que non. ',
+    poster_path: './posters/emmanuelle.jpg',
+    title: 'Emmanuelle',
+  },
+  {
+    backdrop_path: './posters/emmanuelle.jpg',
+    id: 4,
+    overview:
+      'Emmanuelle s\'envole de Paris pour Bangkok afin d\'y rejoindre Jean, son mari qui occupe un poste de diplomate ; lorsqu\'il lui demande si elle a eu des amants pendant qu\'elle était seule à Paris, elle lui affirme que non. ',
+    poster_path: './posters/emmanuelle.jpg',
+    title: 'Emmanuelle',
+  },
+];
