e2bd5d7fb6fa63d8e1843ece445a8f632a357827
Simon Rulquin <simon.rulquin@gmail.com>
2020-02-17 15:48:51 +0100

    Css Home, Header, Creation SearchBar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,14 @@ import './header.scss';
 
 import logo from 'src/images/netflux-logo.svg';
 import pop from 'src/images/pop-corn.svg';
+import SearchBar from './searchBar';
 
 
 const Header = () => (
-  <div className="main-header">
-    <div className="container">
-      <img src={logo} className="logo" alt="logo" />
-      <img src={pop} className="pop" alt="pop" />
-    </div>
+  <div className="header">
+    <img src={logo} className="logo" alt="logo" />
+    <SearchBar />
+    <img src={pop} className="pop" alt="pop" />
   </div>
 );
 
---
+++ src/components/Header/searchBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './searchbar.scss';
+
+const SearchBar = () => (
+  <div className="searchbar-container">
+    <input className="searchbar" type="text" placeholder="Search" />
+  </div>
+);
+
+export default SearchBar;
