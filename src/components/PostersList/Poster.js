ee7e1d86cb4304da2ce00a63a6abc6e4b2bc71d3
Simon Rulquin <simon.rulquin@gmail.com>
2020-02-17 16:11:55 +0100

    creation PostersList, Poster

---
+++ src/components/PostersList/Poster.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Poster = () => (
+  <div className="poster">
+    <h3>
+      Yallah
+    </h3>
+  </div>
+);
+
+export default Poster;
---
+++ src/components/PostersList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './posterslist.scss';
+import Poster from './Poster';
+
+const PostersList = () => (
+  <div className="posterlist">
+    <Poster />
+  </div>
+);
+
+export default PostersList;
