70e3af2085a5a5e9e3b0247ecbe458c4d59d1e00
Simon Rulquin <simon.rulquin@gmail.com>
2020-02-29 20:51:54 +0100

    CSS GLAMOR :hover

--- src/components/PostersList/Poster.js
+++ src/components/PostersList/Poster.js
@@ -1,13 +1,19 @@
 /* eslint-disable no-unused-expressions */
 import React from 'react';
 import { string } from 'prop-types';
+import { css } from 'glamor';
 
 import './poster.scss';
 
-const Poster = ({ title }) => {
+const styles = css({
+  ':hover': {
+    color: 'pink',
+  },
+});
 
+const Poster = ({ title }) => {
   if (title.length <= 5) {
-    return <h3 className="poster-little">{title}</h3>;
+    return <h3 {...styles} className="poster-little">{title}</h3>;
   } if (title.length > 5 && title.length <= 10) {
     return <h3 className="poster-middle">{title}</h3>;
   } if (title.length > 10 && title.length <= 15) {
